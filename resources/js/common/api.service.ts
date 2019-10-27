7365b37d62c6adb0eee58572c6ad02afced0fe58
andrey <lebedev.andrey1894@gmail.com>
2019-10-27 16:14:54 +0300

    ref: ApiService

--- resources/js/common/api.service.ts
+++ resources/js/common/api.service.ts
@@ -1,3 +1,78 @@
 
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 export default class ApiService {
+    private _apiPrefix = 'api'
+
+    init(): void {
+        Vue.use(VueAxios, axios)
+    }
+
+    get apiPrefix(): string {
+        return this._apiPrefix
+    }
+
+    // TODO: Add token in headers
+    setHeader() {
+        // Vue.axios.defaults.headers.common[
+        //   "Authorization"
+        // ] = `Token ${JwtService.getToken()}`;
+      }
+    
+      async query(resource: string, params: any) {
+        try {
+              return Vue.axios.get(resource, params);
+          }
+          catch (error) {
+              console.log(`[RWV] QUERY ApiService ${error}`);
+          }
+      }
+    
+      async get(resource: string, slug = "") {
+        try {
+              return Vue.axios.get(`${resource}/${slug}`);
+          }
+          catch (error) {
+            console.log(`[RWV] GET ApiService ${error}`);
+          }
+      }
+    
+      post(resource: string, params: any) {
+        try {
+            return Vue.axios.post(`${resource}`, params);
+        }
+        catch (error) {
+            console.log(`[RWV] POST ApiService ${error}`)
+        }
+      }
+    
+      update(resource: string, slug: string, params:any) {
+        try {
+            return Vue.axios.put(`${resource}/${slug}`, params);
+        }
+        catch (error) {
+            console.log(`[RWV] UPDATE ApiService ${error}`)
+        }
+      }
+    
+      put(resource: string, params: any) {
+        try {
+            return Vue.axios.put(`${resource}`, params);
+        }
+        catch (error) {
+            console.log(`[RWV] PUT ApiService ${error}`)
+        }
+      }
+    
+      delete(resource: string) {
+        try {
+            return Vue.axios.delete(resource);
+        }
+        catch (error) {
+            console.log(`[RWV] DELETE ApiService ${error}`)
+        }
+      }
+
 }
