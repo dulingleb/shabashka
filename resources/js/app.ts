c8188c168f52921cb34f10cd4a9c2d9ad359c736
andrey <lebedev.andrey1894@gmail.com>
2019-10-27 16:52:20 +0300

    fet: view users via userService

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,12 +7,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import router from './app.router'
+import apiService from './common/api.service'
 import App from './views/App.vue'
 
 Vue.use(NavbarPlugin)
 Vue.use(FormPlugin)
 Vue.use(FormInputPlugin)
 Vue.use(ButtonPlugin)
+
+apiService.init();
 
 const app = new Vue({
   el: '#app',
--- resources/js/common/user.service.ts
+++ resources/js/common/user.service.ts
@@ -1,13 +1,22 @@
 import User from './model/user.model';
+import apiService from './api.service';
 
-export default class UserService {
-    private _user: User
+class UserService {
+    private _user: User // TODO: Current auth user
 
     get user(): User {
         return this._user
     }
 
     async getTestUsers(): Promise<User[]> {
+        const users = await apiService.get('users');
+        if (users && users.length) {
+            return users;
+        }
         return [];
     }
 }
+
+const userService = new UserService();
+
+export default userService;
