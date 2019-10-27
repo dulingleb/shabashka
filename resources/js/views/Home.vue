081b471da7d1222652008552b2a9f58255356b7a
andrey <lebedev.andrey1894@gmail.com>
2019-10-27 15:49:13 +0300

    feat: add test route api/users

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -4,15 +4,52 @@
           <app-aside></app-aside>
         </div>
         <div class="col-md-8 col-lg-9">
-           CONTENT
+           <div class="users">
+    <div class="loading" v-if="loading">
+      Loading...
+    </div>
+
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+
+    <ul v-if="users">
+      <li v-for="{ name, email } in users">
+        <strong>Name:</strong> {{ name }},
+        <strong>Email:</strong> {{ email }}
+      </li>
+    </ul>
+  </div>
         </div>
       </div>
 </template>
 
 <script lang="ts">
 import Aside from "../common/components/Aside.vue";
+import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      loading: false,
+      users: null,
+      error: null,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.error = this.users = null;
+      this.loading = true;
+      axios
+        .get('/api/users')
+        .then(response => {
+          console.log(response);
+        });
+    }
+  },
   components: {
     "app-aside": Aside,
   }
