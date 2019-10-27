14c5149b293f604642324e574877a24df5a0f6ba
andrey <lebedev.andrey1894@gmail.com>
2019-10-27 15:17:13 +0300

    feat: Add header, footer. Ref: common structure

---
+++ resources/js/common/components/Aside.vue
@@ -0,0 +1,16 @@
+ <template>
+  <aside class="bg-light">
+      ASIDE
+  </aside>
+</template>
+
+<script lang="ts">
+export default {
+  name: "app-aside",
+  components: {}
+};
+</script> 
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,29 +1,34 @@
  <template>
-  <div>
+  <div class="app-container">
     <app-header></app-header>
-    <h1>Vue Router Demo App</h1>
 
-    <p>
-      <router-link :to="{ name: 'home' }">Home</router-link> |
-      <router-link :to="{ name: 'hello' }">Hello World</router-link>
-    </p>
-
-    <div class="container">
+    <main class="container">
       <router-view></router-view>
-    </div>
+    </main>
+
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script lang="ts">
-  import Header from '../common/components/Header.vue';
+import Header from "../common/components/Header.vue";
+import Footer from "../common/components/Footer.vue";
 
-  export default {
-    components: {
-      'app-header': Header
-    }
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
   }
+};
 </script> 
 
 <style lang="scss" scoped>
-
+  .app-container {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    > .container {
+      flex-grow: 1;
+    }
+  }
 </style>
