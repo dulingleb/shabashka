0eb1673609e58bb1c8f43f1694df522a2c0c2c54
andrey <lebedev.andrey1894@gmail.com>
2019-10-27 15:31:08 +0300

    feat: add Login & Register pages

---
+++ resources/js/views/Reg.vue
@@ -0,0 +1,15 @@
+ <template>
+  <div>
+      Register page
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "app-register",
+  components: {}
+};
+</script> 
+
+<style lang="scss" scoped>
+</style>
