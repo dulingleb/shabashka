45bb21b9dada7e5c130d726771245427e6026158
Gleb <dulingleb@gmail.com>
2019-12-24 19:01:05 +0300

    Chore: 1) /api/me update is ready

--- app/Company.php
+++ app/Company.php
@@ -8,6 +8,16 @@ class Company extends Model
 {
     protected $fillable = ['user_id', 'is_active', 'is_moderate', 'inn', 'description', 'title', 'address', 'documents'];
 
+    public function getDocumentsAttribute($value){
+        $_v = unserialize($value);
+        $files = [];
+        foreach ($_v as $file){
+            $files[] = 'storage/users/' . $this->user_id . '/' . $file;
+        }
+
+        return json_encode($files);
+    }
+
     public function categories(){
         return $this->morphToMany('App\Category', 'categoriable');
     }
