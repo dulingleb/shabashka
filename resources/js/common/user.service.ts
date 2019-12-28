ff9ca81d29a2c3a0d28a2a056db0ba3060faac47
andrey <lebedev.andrey1894@gmail.com>
2019-12-28 13:52:20 +0300

    fix: set isActive

--- resources/js/common/user.service.ts
+++ resources/js/common/user.service.ts
@@ -86,6 +86,7 @@ class UserService {
     if (!company) return bodyFormData
     const newCompany = []
     try {
+      newCompany['is_active'] = company.isActive
       if (company.title && company.title.length) newCompany['title'] = company.title
       if (company.inn && company.inn.length) newCompany['inn'] = company.inn
       if (company.description && company.description.length) newCompany['description'] = company.description
